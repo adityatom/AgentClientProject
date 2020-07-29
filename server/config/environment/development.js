e9447a283605e1fc7cc1e307cf67480b1f107b06
adityatom <nagamalliaditya3@gmail.com>
2020-07-29 19:16:04 +0530

    url  changes

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,8 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb+srv://user123:User@123@clusterarc.qmiom.mongodb.net/test'
+   uri:'mongodb+srv://user123:user123@clusterarc.qmiom.mongodb.net/test?retryWrites=true&w=majority'
+  //  uri: 'mongodb+srv://user123:User@123@clusterarc.qmiom.mongodb.net/test'
   },
 
   seedDB: false
